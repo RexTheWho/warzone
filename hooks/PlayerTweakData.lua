b7f495e754dcebe2f22ea6879dba95f9f24a6031
RexTheWho <jrcrex1@gmail.com>
2020-08-12 04:01:02 +0800

    Tweaked Parachute

--- hooks/PlayerTweakData.lua
+++ hooks/PlayerTweakData.lua
@@ -1,5 +1,5 @@
 Hooks:PostHook(PlayerTweakData,"_init_parachute","wz_init",function(self)
-	self.parachute.movement.forward_speed = 200
-	self.parachute.terminal_velocity = 700
+	self.parachute.movement.forward_speed = 450
+	self.parachute.terminal_velocity = 550
 	self.parachute.cooldown = 30
 end)
